b5c8ff22005fd1b79eb95424d55d53b1c6e7f6e3
Ranger234 <ranger234@hotmail.de>
2022-09-12 19:50:35 +0200

    robots und reset hinzu

---
+++ reset.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+datei1="/kunden/homepages/1/d88040495/htdocs/OpenCaching/OC15A33/cachcode1.txt"
+datei2="/kunden/homepages/1/d88040495/htdocs/OpenCaching/OC15A33/cachcode2.txt"
+datei3="/kunden/homepages/1/d88040495/htdocs/OpenCaching/OC15A33/zeitstempel.txt"
+jetzt=$(date "+%s")
+if test -f ; then
+    alter1=$(date -r $datei1 "+%s")
+    unter1=$(( jetzt - alter1 ))
+    if [ $unter1 -gt 180 ];
+       then 
+           rm $datei1
+       fi
+fi
+if test -f $datei2; then
+    alter2=$(date -r $datei2 "+%s")
+    unter2=$(( jetzt - alter2 ))
+    if [ $unter2 -gt 180 ];
+       then 
+           rm $datei2
+       fi
+fi
+if test -f $datei3; then
+    alter3=$(date -r $datei3 "+%s")
+    unter3=$(( jetzt - alter3 ))
+    if [ $unter2 -gt 180 ];
+       then 
+           rm $datei3
+       fi
+fi
+exit
